c57d8772528df7469eec0018521aad78e131f846
quangtoan1312 <haquangtoan99@gmail.com>
2022-07-06 17:33:34 +0700

    update

---
+++ ASP.NET-MVC-CRUD-Toanhq/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        }
+
+
+    }
+}
---
+++ ASP.NET-MVC-CRUD-Toanhq/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC_CRUD_Toanhq.MvcApplication" Language="C#" %>
